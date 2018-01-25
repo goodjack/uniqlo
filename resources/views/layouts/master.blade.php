449484f503df898ffbf04c0e265333918155ae8a
Jack <jack_lin@intentarget.com>
2018-01-25 18:02:24 +0800

    Add responsive meta tag

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
         @include('layouts.google-analytics')
 
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <title>@yield('title') - UQ 搜尋</title>
 
