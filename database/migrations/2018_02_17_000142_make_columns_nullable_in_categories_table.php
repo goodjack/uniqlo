abf3c9ed0eda5a5503d717ce9a6c9cd2576cd095
小克 <mirumodepon@gmail.com>
2018-02-17 00:17:59 +0800

    Make columns nullable in categories table

---
+++ database/migrations/2018_02_17_000142_make_columns_nullable_in_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeColumnsNullableInCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('name')->nullable()->change();
+            $table->string('image')->nullable()->change();
+            $table->integer('level')->nullable()->change();
+            $table->integer('weight')->nullable()->change();
+            $table->string('parent_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('name')->nullable(false)->change();
+            $table->string('image')->nullable(false)->change();
+            $table->integer('level')->nullable(false)->change();
+            $table->integer('weight')->nullable(false)->change();
+            $table->string('parent_id')->nullable(false)->change();
+        });
+    }
+}
