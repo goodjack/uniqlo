8dec96e7b6a3229567cde11a08264adf8de46a76
小克 <mirumodepon@gmail.com>
2018-03-04 16:32:04 +0800

    Fix style dictionaries URLs error

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -67,8 +67,8 @@ class ProductRepository extends Repository
             
             $model->id = $detail->id;
             $model->fnm = $detail->fnm;
-            $model->image_url = "http://www.uniqlo.com/tw/stylingbook/detail.html#/detail/{$model->id}";
-            $model->detail_url = "http://www.uniqlo.com/{$imgDir}{$model->fnm}-xl.jpg";
+            $model->image_url = "http://www.uniqlo.com/{$imgDir}{$model->fnm}-xl.jpg";
+            $model->detail_url = "http://www.uniqlo.com/tw/stylingbook/detail.html#/detail/{$model->id}";
 
             $model->save();
         } catch (Exception $e) {
