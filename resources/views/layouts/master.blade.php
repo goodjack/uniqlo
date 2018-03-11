34c7c54b9fa527fd37d23d06c20f7a1115c354f4
小克 <mirumodepon@gmail.com>
2018-03-11 13:24:04 +0800

    Add theme color meta tag

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="theme-color" content="#ce5e57">
 
         <title>@yield('title') - UQ 搜尋</title>
 
