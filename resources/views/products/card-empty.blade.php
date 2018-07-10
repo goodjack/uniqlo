5eaee4ac2b4db55329060421a180f48f8613d231
小克 <mirumodepon@gmail.com>
2018-07-10 23:29:47 +0800

    Add the empty sub-view and change layouts

---
+++ resources/views/products/card-empty.blade.php
@@ -0,0 +1,5 @@
+<div class="ts basic slate">
+    <i class="frown faded icon"></i>
+    <span class="header">沒有結果</span>
+    <span class="description">請試試看其他關鍵字</span>
+</div>
--- resources/views/products/cards.blade.php
+++ resources/views/products/cards.blade.php
@@ -1,30 +1,62 @@
 <div class="ts attached padded horizontally fitted fluid segment">
     <div class="ts narrow container">
         <div id="category" class="ts top attached four item tabbed menu">
-            <a class="active item" data-tab="Men">男裝 ({{ count($products['men']) }})</a>
-            <a class="item" data-tab="Women">女裝 ({{ count($products['women']) }})</a>
-            <a class="item" data-tab="Kids">童裝 ({{ count($products['kids']) }})</a>
-            <a class="item" data-tab="Baby">嬰幼兒 ({{ count($products['baby']) }})</a>
+            <a class="active item" data-tab="Men">MEN</a>
+            <a class="item" data-tab="Women">WOMEN</a>
+            <a class="item" data-tab="Kids">KIDS</a>
+            <a class="item" data-tab="Baby">BABY</a>
         </div>
         <div data-tab="Men" class="ts active bottom attached tab segment">
+            <div class="ts large header">
+                男裝
+                <div class="inline sub header">共 {{ count($products['men']) }} 件</div>
+            </div>
+            @if (count($products['men']) > 0)
             <div class="ts doubling link cards four">
                 @each('products.card', $products['men'], 'product')
             </div>
+            @else
+                @include('products.card-empty')
+            @endif
         </div>
         <div data-tab="Women" class="ts bottom attached tab segment">
+            <div class="ts large header">
+                女裝
+                <div class="inline sub header">共 {{ count($products['women']) }} 件</div>
+            </div>
+            @if (count($products['women']) > 0)
             <div class="ts doubling link cards four">
                 @each('products.card', $products['women'], 'product')
             </div>
+            @else
+                @include('products.card-empty')
+            @endif
         </div>
         <div data-tab="Kids" class="ts bottom attached tab segment">
+            <div class="ts large header">
+                童裝
+                <div class="inline sub header">共 {{ count($products['kids']) }} 件</div>
+            </div>
+            @if (count($products['kids']) > 0)
             <div class="ts doubling link cards four">
                 @each('products.card', $products['kids'], 'product')
             </div>
+            @else
+                @include('products.card-empty')
+            @endif
         </div>
         <div data-tab="Baby" class="ts bottom attached tab segment">
+            <div class="ts large header">
+                嬰幼兒
+                <div class="inline sub header">共 {{ count($products['baby']) }} 件</div>
+            </div>
+            @if (count($products['baby']) > 0)
             <div class="ts doubling link cards four">
                 @each('products.card', $products['baby'], 'product')
             </div>
+            @else
+                @include('products.card-empty')
+            @endif
         </div>
     </div>
 </div>
