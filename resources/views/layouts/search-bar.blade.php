12d7ab3bd4f1de35a438a69cea01983f6a0c521d
小克 <goodjack@users.noreply.github.com>
2020-02-02 18:08:29 +0800

    Give the search button an accessible name

--- resources/views/layouts/search-bar.blade.php
+++ resources/views/layouts/search-bar.blade.php
@@ -1,6 +1,6 @@
 <form class="ts fluid action input" action="{{ route('products.go') }}">
     <input name="id" type="number" placeholder="輸入 UNIQLO 商品編號 ...">
-    <button class="ts basic icon button" type="submit">
+    <button class="ts basic icon button" type="submit" aria-label="Search">
         <i class="arrow right icon"></i>
     </button>
 </form>
