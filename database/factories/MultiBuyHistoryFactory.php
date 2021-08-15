8d9f59661740d4daea2d60aacf8cb8fc9edc6833
小克 <goodjack@users.noreply.github.com>
2021-08-15 21:48:32 +0800

    fix: Rewrite factories to support Laravel 8 model factories

--- database/factories/MultiBuyHistoryFactory.php
+++ database/factories/MultiBuyHistoryFactory.php
@@ -1,14 +1,32 @@
 <?php
 
+namespace Database\Factories;
+
 use App\Models\MultiBuyHistory;
 use App\Models\Product;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(MultiBuyHistory::class, function (Faker $faker) {
-    return [
-        'product_id' => function () {
-            return Product::factory()->create()->id;
-        },
-        'multi_buy' => "購買{$faker->randomNumber(1)}件NT\${$faker->randomNumber(2)}0.00",
-    ];
-});
+class MultiBuyHistoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = MultiBuyHistory::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'product_id' => function () {
+                return Product::factory()->create()->id;
+            },
+            'multi_buy' => "購買{$this->faker->randomNumber(1)}件NT\${$this->faker->randomNumber(2)}0.00",
+        ];
+    }
+}
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,33 +1,51 @@
 <?php
 
+namespace Database\Factories;
+
 use App\Models\Product;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(Product::class, function (Faker $faker) {
-    $category1 = '00'.$faker->numberBetween(1, 4);
-    $category2 = '00'.$faker->numberBetween(1, 9);
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
 
-    return [
-        'id' => $faker->randomNumber(6, true),
-        'name' => $faker->sentence,
-        'category_id' => $category = $category1.$category2.$faker->randomNumber(3, true),
-        'categories' => "[\"{$category}\"]",
-        'ancestors' => "[\"{$category1}\",\"{$category1}{$category2}\",\"{$category}\"]",
-        'main_image_url' => $faker->imageUrl,
-        'comment' => $faker->paragraph,
-        'min_price' => $minPrice = $faker->numberBetween(39, 6990),
-        'max_price' => $maxPrice = $faker->numberBetween($minPrice, 6990),
-        'price' => $faker->numberBetween($minPrice, $maxPrice),
-        'flags' => '[{"name":"ONLINE_ONLY_LIMITED_OFFER","value":"ONLINE_ONLY_LIMITED_OFFER"}]',
-        'representative_sku_flags' => '[{"name":"ONLINE_ONLY_LIMITED_OFFER","value":"ONLINE_ONLY_LIMITED_OFFER"}]',
-        'limit_sales_end_msg' => $faker->sentence,
-        'multi_buy' => $faker->sentence,
-        'new' => $faker->boolean,
-        'sale' => $faker->boolean,
-        'stockout' => $faker->boolean,
-        'review_count' => $faker->randomNumber(1),
-        'colors' => "[{\"name\":\"{$faker->colorName}\",\"code\":\"{$faker->randomNumber(2, true)}\"},{\"name\":\"{$faker->colorName}\",\"code\":\"{$faker->randomNumber(2, true)}\"}]",
-        'sub_images' => "[\"182500_sub5\"]",
-        'style_dictionary_images' => null,
-    ];
-});
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $category1 = '00' . $this->faker->numberBetween(1, 4);
+        $category2 = '00' . $this->faker->numberBetween(1, 9);
+
+        return [
+            'id' => $this->faker->randomNumber(6, true),
+            'name' => $this->faker->sentence,
+            'category_id' => $category = $category1 . $category2 . $this->faker->randomNumber(3, true),
+            'categories' => "[\"{$category}\"]",
+            'ancestors' => "[\"{$category1}\",\"{$category1}{$category2}\",\"{$category}\"]",
+            'main_image_url' => $this->faker->imageUrl,
+            'comment' => $this->faker->paragraph,
+            'min_price' => $minPrice = $this->faker->numberBetween(39, 6990),
+            'max_price' => $maxPrice = $this->faker->numberBetween($minPrice, 6990),
+            'price' => $this->faker->numberBetween($minPrice, $maxPrice),
+            'flags' => '[{"name":"ONLINE_ONLY_LIMITED_OFFER","value":"ONLINE_ONLY_LIMITED_OFFER"}]',
+            'representative_sku_flags' => '[{"name":"ONLINE_ONLY_LIMITED_OFFER","value":"ONLINE_ONLY_LIMITED_OFFER"}]',
+            'limit_sales_end_msg' => $this->faker->sentence,
+            'multi_buy' => $this->faker->sentence,
+            'new' => $this->faker->boolean,
+            'sale' => $this->faker->boolean,
+            'stockout' => $this->faker->boolean,
+            'review_count' => $this->faker->randomNumber(1),
+            'colors' => "[{\"name\":\"{$this->faker->colorName}\",\"code\":\"{$this->faker->randomNumber(2, true)}\"},{\"name\":\"{$this->faker->colorName}\",\"code\":\"{$this->faker->randomNumber(2, true)}\"}]",
+            'sub_images' => '["182500_sub5"]',
+            'style_dictionary_images' => null,
+        ];
+    }
+}
