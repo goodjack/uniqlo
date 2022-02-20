722f9b249dbfdd844a43dc9083e3cf496c944bef
小克 <goodjack@users.noreply.github.com>
2022-02-20 20:39:09 +0800

    feat: permanently redirect the old lists to the new lists

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,12 @@ Route::get('/', function () {
 Route::group(['prefix' => 'products'], function () {
     Route::get('/', 'ProductController@index')->name('products.index');
     Route::get('/stockouts', 'ProductController@stockouts')->name('products.stockouts');
-    Route::get('/sales', 'ProductController@sales')->name('products.sales');
-    Route::get('/limited-offers', 'ProductController@limitedOffers')->name('products.limited-offers');
-    Route::get('/multi-buys', 'ProductController@multiBuys')->name('products.multi-buys');
-    Route::get('/news', 'ProductController@news')->name('products.news');
+    Route::permanentRedirect('/sales', '/lists/sale')->name('products.sales');
+    Route::permanentRedirect('/limited-offers', '/lists/limited-offers')->name('products.limited-offers');
+    Route::permanentRedirect('/multi-buys', '/lists/multi-buy')->name('products.multi-buys');
+    Route::permanentRedirect('/news', '/lists/new')->name('products.news');
+    Route::permanentRedirect('/most-reviewed', '/lists/most-reviewed')->name('products.most-reviewed');
     Route::get('/go', 'ProductController@go')->name('products.go');
-    Route::get('/most-reviewed', 'ProductController@mostReviewed')->name('products.most-reviewed');
     Route::get('/{product}', 'ProductController@show')->name('products.show');
 });
 
@@ -51,6 +51,14 @@ Route::group(['prefix' => 'lists'], function () {
     Route::get('/coming-soon', 'ListController@getComingSoon')->name('lists.coming-soon');
     Route::get('/multi-buy', 'ListController@getMultiBuy')->name('lists.multi-buy');
     Route::get('/online-special', 'ListController@getOnlineSpecial')->name('lists.online-special');
+});
+
+Route::group(['prefix' => 'old-lists'], function () {
+    Route::get('/limited-offers', 'ProductController@limitedOffers')->name('old-lists.limited-offers');
+    Route::get('/sales', 'ProductController@sales')->name('old-lists.sales');
+    Route::get('/most-reviewed', 'ProductController@mostReviewed')->name('old-lists.most-reviewed');
+    Route::get('/news', 'ProductController@news')->name('old-lists.news');
+    Route::get('/multi-buys', 'ProductController@multiBuys')->name('old-lists.multi-buys');
 });
 
 // Auth::routes();
