1fdbdcfa6764c15db443b005d8a58b0d9e81d2c7
小克 <goodjack@users.noreply.github.com>
2022-06-11 19:30:12 +0800

    fix: Support more types of StyleHint

--- app/StyleHint.php
+++ app/StyleHint.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 class StyleHint extends Model
@@ -16,17 +17,20 @@ class StyleHint extends Model
 
     public function getStyleImageUrlAttribute($value)
     {
-        preg_match('/([a-z]+)([0-9]+)/', $value, $matches);
+        preg_match('/^([a-z]+)([0-9]+)(gu)?(Igbiz|Olapic)?([0-9]*)$/', $value, $matches);
 
-        if (count($matches) !== 3) {
+        if (count($matches) < 3) {
             return $value;
         }
 
-        $country = $matches[1];
-        $number = $matches[2];
-        $originalOutfitId = $this->getRawOriginal('original_source_url');
+        $matches = collect($matches);
 
-        return "https://api.fastretailing.com/ugc/v1/uq/{$country}/SR_IMAGES/ugc_stylehint_uq_{$country}_photo_{$number}_{$originalOutfitId}";
+        $country = $matches->get(1);
+        $number = $matches->get(2);
+        $brand = $matches->get(3) ?: 'uq';
+        [$type, $originalOutfitId] = $this->getTypeAndOriginalOutfitId($matches);
+
+        return "https://api.fastretailing.com/ugc/v1/{$brand}/{$country}/SR_IMAGES/ugc_{$type}_{$brand}_{$country}_photo_{$number}_{$originalOutfitId}";
     }
 
     public function getOriginalSourceUrlAttribute($value)
@@ -57,13 +61,17 @@ class StyleHint extends Model
         $styleImageUrl = $value;
 
         preg_match(
-            '|https://api.fastretailing.com/ugc/v1/uq/([a-z]+)/SR_IMAGES/ugc_stylehint_uq_[a-z]+_photo_([0-9]+)_[0-9]+$|',
+            '/https:\/\/api.fastretailing.com\/ugc\/v1\/(uq|gu)\/([a-z]+)\/SR_IMAGES\/ugc_(stylehint|instagram-business|olapic)_(?:uq|gu)_[a-z]+_photo_([0-9]+)_([0-9]+)$/',
             $value,
             $matches
         );
 
-        if (count($matches) === 3) {
-            $styleImageUrl = "{$matches[1]}{$matches[2]}";
+        if (count($matches) === 6) {
+            $suffix = $matches[1] === 'uq' ? '' : 'gu';
+            $suffix .= $matches[3] === 'instagram-business' ? "Igbiz{$matches[5]}" : '';
+            $suffix .= $matches[3] === 'olapic' ? "Olapic{$matches[5]}" : '';
+
+            $styleImageUrl = "{$matches[2]}{$matches[4]}{$suffix}";
         }
 
         $this->attributes['style_image_url'] = $styleImageUrl;
@@ -78,5 +86,18 @@ class StyleHint extends Model
         );
 
         $this->attributes['original_source_url'] = $matches[1] ?? $value;
+    }
+
+    private function getTypeAndOriginalOutfitId(Collection $matches)
+    {
+        if ($matches->get(4) === 'Igbiz') {
+            return ['instagram-business', $matches->get(5)];
+        }
+
+        if ($matches->get(4) === 'Olapic') {
+            return ['olapic', $matches->get(5)];
+        }
+
+        return ['stylehint', $this->getRawOriginal('original_source_url')];
     }
 }
---
+++ tests/Unit/Models/StyleHintTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\StyleHint;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class StyleHintTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function test_user_can_set_and_get_StyleHint_attributes(
+        $country,
+        $styleImageUrlFromApi,
+        $styleImageUrlFromDb,
+        $originalSourceUrlFromApi,
+        $originalSourceUrlFromDb
+    ) {
+        $model = new StyleHint();
+
+        $model->country = $country;
+        $model->outfit_id = $this->faker->randomNumber(7, false);
+        $model->style_image_url = $styleImageUrlFromApi;
+        $model->original_source_url = $originalSourceUrlFromApi;
+
+        $model->save();
+
+        $this->assertEquals($styleImageUrlFromApi, $model->style_image_url);
+        $this->assertEquals($originalSourceUrlFromApi, $model->original_source_url);
+
+        $this->assertEquals($styleImageUrlFromDb, $model->getRawOriginal('style_image_url'));
+        $this->assertEquals($originalSourceUrlFromDb, $model->getRawOriginal('original_source_url'));
+    }
+
+    public function dataProvider()
+    {
+        return [
+            'uq_jp_stylehint' => [
+                'country' => 'jp',
+                'styleImageUrlFromApi' => 'https://api.fastretailing.com/ugc/v1/uq/jp/SR_IMAGES/ugc_stylehint_uq_jp_photo_991231_111111',
+                'styleImageUrlFromDb' => 'jp991231',
+                'originalSourceUrlFromApi' => 'https://www.stylehint.com/jp/ja/outfit/111111',
+                'originalSourceUrlFromDb' => '111111',
+            ],
+            'gu_jp_stylehint' => [
+                'country' => 'jp',
+                'styleImageUrlFromApi' => 'https://api.fastretailing.com/ugc/v1/gu/jp/SR_IMAGES/ugc_stylehint_gu_jp_photo_991231_111111',
+                'styleImageUrlFromDb' => 'jp991231gu',
+                'originalSourceUrlFromApi' => 'https://www.stylehint.com/jp/ja/outfit/111111',
+                'originalSourceUrlFromDb' => '111111',
+            ],
+            'uq_us_stylehint' => [
+                'country' => 'us',
+                'styleImageUrlFromApi' => 'https://api.fastretailing.com/ugc/v1/uq/us/SR_IMAGES/ugc_stylehint_uq_us_photo_991231_111111',
+                'styleImageUrlFromDb' => 'us991231',
+                'originalSourceUrlFromApi' => 'https://www.stylehint.com/us/en/outfit/111111',
+                'originalSourceUrlFromDb' => '111111',
+            ],
+            'gu_us_stylehint' => [
+                'country' => 'us',
+                'styleImageUrlFromApi' => 'https://api.fastretailing.com/ugc/v1/gu/us/SR_IMAGES/ugc_stylehint_gu_us_photo_991231_111111',
+                'styleImageUrlFromDb' => 'us991231gu',
+                'originalSourceUrlFromApi' => 'https://www.stylehint.com/us/en/outfit/111111',
+                'originalSourceUrlFromDb' => '111111',
+            ],
+            'uq_us_instagram-business' => [
+                'country' => 'us',
+                'styleImageUrlFromApi' => 'https://api.fastretailing.com/ugc/v1/uq/us/SR_IMAGES/ugc_instagram-business_uq_us_photo_991231_111111',
+                'styleImageUrlFromDb' => 'us991231Igbiz111111',
+                'originalSourceUrlFromApi' => 'https://www.instagram.com/p/C/',
+                'originalSourceUrlFromDb' => 'https://www.instagram.com/p/C/',
+            ],
+            'uq_us_olapic' => [
+                'country' => 'us',
+                'styleImageUrlFromApi' => 'https://api.fastretailing.com/ugc/v1/uq/us/SR_IMAGES/ugc_olapic_uq_us_photo_991231_111111',
+                'styleImageUrlFromDb' => 'us991231Olapic111111',
+                'originalSourceUrlFromApi' => 'https://www.instagram.com/p/C/',
+                'originalSourceUrlFromDb' => 'https://www.instagram.com/p/C/',
+            ],
+        ];
+    }
+}
