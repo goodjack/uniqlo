151902f441d8aebe30e843043d45058a9ea9fdaf
小克 <goodjack@users.noreply.github.com>
2022-06-30 02:00:47 +0800

    feat: Change the route of StyleHint pages

--- routes/web.php
+++ routes/web.php
@@ -30,14 +30,11 @@ Route::group(['prefix' => 'products'], function () {
 
 Route::group(['prefix' => 'hmall-products'], function () {
     Route::get('/{uniqlo_product_code}', 'HmallProductController@show')->name('uniqlo-hmall-products.show');
+    Route::get('/{uniqlo_product_code}/style-hints', 'StyleHintController@show')->name('style-hints.show');
 });
 
 Route::group(['prefix' => 'gu-products'], function () {
     Route::get('/{gu_product_code}', 'HmallProductController@show')->name('gu-hmall-products.show');
-});
-
-Route::group(['prefix' => 'style-hints'], function () {
-    Route::get('/{uniqlo_product_code}', 'StyleHintController@show')->name('style-hints.show');
 });
 
 Route::group(['prefix' => 'search'], function () {
