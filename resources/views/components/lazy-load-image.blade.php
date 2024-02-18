3f278a5696db590cfb5bf356e5fab42f0b643968
小克 <goodjack@users.noreply.github.com>
2024-02-18 15:04:31 +0800

    feat: Add aspect-ratio to solve the CLS issues

--- resources/views/components/lazy-load-image.blade.php
+++ resources/views/components/lazy-load-image.blade.php
@@ -1,3 +1,3 @@
 <img data-src="{{ $src }}" alt="{{ $alt }}" loading="lazy"
     src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 {{ $width }} {{ $height }}'%3E%3C/svg%3E"
-    {{ $attributes->merge(['class' => 'lazyload']) }}>
+    {{ $attributes->merge(['class' => 'lazyload']) }} style="aspect-ratio: {{ $width }} / {{ $height }};">
