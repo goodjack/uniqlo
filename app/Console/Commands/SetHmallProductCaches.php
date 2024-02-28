1681ad63ec47e5d498672bd04e7a89d84ff058cb
小克 <goodjack@users.noreply.github.com>
2024-02-29 01:10:30 +0800

    feat: Schedule daily fetching of Japan products

--- app/Console/Commands/SetHmallProductCaches.php
+++ app/Console/Commands/SetHmallProductCaches.php
@@ -47,6 +47,7 @@ class SetHmallProductCaches extends Command
         $this->hmallProductRepository->setLimitedOfferHmallProductsCache();
         $this->hmallProductRepository->setSaleHmallProductsCache();
         $this->hmallProductRepository->setMostReviewedHmallProductsCache();
+        $this->hmallProductRepository->setJapanMostReviewedHmallProductsCache();
         $this->hmallProductRepository->setTopWearingHmallProductsCache();
         $this->hmallProductRepository->setNewHmallProductsCache();
         $this->hmallProductRepository->setComingSoonHmallProductsCache();
